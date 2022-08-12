aaae9f2cc21312be8ba48a07408d1d89ff758556
g1ovanaqueiroz <giovana.oliveira@ccc.ufcg.edu.br>
2022-08-11 22:54:50 -0300

    add fold and compose function

--- functional-project-frontend/src/app/app.component.ts
+++ functional-project-frontend/src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { group } from '@angular/animations';
 import { Component } from '@angular/core';
-import {distinct, group_By, orderBy} from '../utils/utils'
 
 @Component({
   selector: 'app-root',
@@ -10,23 +8,3 @@ import {distinct, group_By, orderBy} from '../utils/utils'
 export class AppComponent {
   title = 'Functional Programming';
 }
-
-
-const peopleDefault = [
-
-  { id: 1, name: 'John', age: 20 },
-  { id: 9, name: 'Luke', age: 21 },
-  { id: 4, name: 'John', age: 23 },
-  { id: 5, name: 'Luke', age: 25 },
-  { id: 8, name: 'Bea', age: 27 },
-  { id: 6, name: 'James', age: 25 },
-  { id: 3, name: 'mary', age: 20 },
-  { id: 7, name: 'Luci', age: 26 },
-  { id: 2, name: 'Jane', age: 21 },
-  { id: 10, name: 'Bryan', age: 29 },
-
-];
-
-console.log(distinct(peopleDefault, "name"));
-console.log(group_By(peopleDefault, "age"));
-console.log(orderBy(peopleDefault, "name"));
--- functional-project-frontend/src/utils/utils.ts
+++ functional-project-frontend/src/utils/utils.ts
@@ -7,20 +7,23 @@
 
     @returns - An array of collection with no duplicates.
 */
-export function distinct<T, K extends keyof T>(collection: T[], attribute: K): any[] {
+export function distinct<T, K extends keyof T>(collection: T[], attribute: K): T[] {
 
     const head = collection[0];
     const tail = collection.slice(1);
 
     if (collection.length === 0) {
-
         return [];
-
     } 
+
     const distinctTail = tail.filter(obj => obj[attribute] !== head[attribute]);
     return [head, ...distinct(distinctTail, attribute)];
 }
 
+/*
+    Receives an object, a collection of objects, and an attribute.
+    The function return indicates whether the attribute of the passed object is present in the received collection.
+*/
 export function elem<T, K extends keyof T>(aValue: any, collection: T[], attribute: K): boolean {
     const result = collection.filter(obj => obj[attribute] === aValue);
     return result.length > 0;
@@ -36,7 +39,7 @@ export function elem<T, K extends keyof T>(aValue: any, collection: T[], attribu
 
     @returns - An array of object groups
 */
-export function group_By<T, K extends keyof T>(collection: T[], attribute: K): any[] {
+export function group_By<T, K extends keyof T>(collection: T[], attribute: K): {}[] {
 
     if (collection.length === 0) {
 
@@ -48,7 +51,7 @@ export function group_By<T, K extends keyof T>(collection: T[], attribute: K): a
       const equal = collection.filter((obj) => obj[attribute] === head[attribute]);
       const different = collection.filter((obj) => obj[attribute] !== head[attribute]);
       let val: any = head[attribute];
-      return [{ [val]: equal }, ...group_By(different, attribute)];
+      return [{[val]: equal}, ...group_By(different, attribute)];
 
     }
   }
@@ -61,16 +64,44 @@ export function group_By<T, K extends keyof T>(collection: T[], attribute: K): a
 
     @returns - an ordered array
 */
-export function orderBy<T, K extends keyof T>(collection: T[], attribute: K): any {
-  // Null is a result for a nullable or undefined array
-  if (!collection) {
-    return null;
+export function orderBy<T, K extends keyof T>(collection: T[], attribute: K): T[] {
+  if (collection.length === 0) {
+    return [];
   }
-
+  
   if (collection.length <= 1) {
     return collection;
   }
 
-  collection.sort((a, b) => (a[attribute] < b[attribute] ? -1 : 1));
-  return collection;
+  const result: T[] = [...collection];
+  result.sort((a, b) => (a[attribute] < b[attribute] ? -1 : 1));
+  return result;
+}
+
+/*
+    Reduces an array of objects to a value
+
+    @param reducer - Reduction function
+    @param init - Initial value of the reduction
+    @param collection -  Object collection to be reduced
+
+    @returns - the final value of the reduction
+*/
+export function fold(reducer: (acc: any, curr: any) => any, init: any, collection: any[]) {
+  return collection.reduce(reducer, init);
+}
+
+/*
+    Represents the composition function (high order), corresponding to compose(f1,f2)(arg) = f1(f2(arg)))
+
+    @param f1 - Composition function 1
+    @param f2 -  Composition function 2
+
+    @returns - compound function
+*/
+export function compose(f1: Function, f2: Function): Function {
+  function composition(...args: any) {
+    return f1(f2(args));
+  };
+  return composition;
 }
